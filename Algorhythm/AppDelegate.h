da589c66eb745cf424c479b0b669203990cee944
codyzazulak1 <codyzazulak1@gmail.com>
2015-06-18 06:33:09 +0000

    Completed

---
+++ Algorhythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorhythm
+//
+//  Created by Cody Zazulak on 2015-06-17.
+//  Copyright (c) 2015 Cody Zazulak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorhythm/PlaylistMasterViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Algorhythm
+//
+//  Created by Cody Zazulak on 2015-06-17.
+//  Copyright (c) 2015 Cody Zazulak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaylistMasterViewController : UIViewController
+
+@property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *playlistImageViews;
+
+@end
+
